a4be12c037cf43117ca7c2f04b4bdcbb22517bcb
viral32111 <pepper1919@hotmail.co.uk>
2017-03-05 11:56:13 +0000

    Updated to 1.0.7
    Fixed bugs todo with the HUD, Added support for SCars and Simfphys Lua
    Vehicles.

--- lua/autorun/carkeys.lua
+++ lua/autorun/carkeys.lua
@@ -1,11 +1,7 @@
 -- Copyright 2017 viral32111. https://github.com/viral32111/car-keys/blob/master/LICENCE
 
-local addonVersion = "1.0.6"
-versionchecked = false
-validVehicles = {
-	"prop_vehicle_jeep",
-	"prop_vehicle_airboat"
-}
+local addonVersion = "1.0.7"
+local versionchecked = false
 
 if ( SERVER ) then
 	print("[Car Keys] Loading Car Keys...")
@@ -16,11 +12,7 @@ if ( SERVER ) then
 end
 
 if ( CLIENT ) then
-	print("[Car Keys] Loading Car Keys...")
-	print("[Car Keys] Author: viral32111")
-	print("[Car Keys] Version: " .. addonVersion )
-
-	print("[Car Keys] Finished loading Car Keys!")
+	print("This server is using Car Keys, a script made by viral32111! (https://www.steamcommunity.com/id/viral32111)")
 end
 
 hook.Add( "PlayerConnect", "CarKeysVersionChecker", function( name, ip )
@@ -44,6 +36,7 @@ hook.Add( "PlayerConnect", "CarKeysVersionChecker", function( name, ip )
 	end
 end )
 
+--[[ Disabled because its a bit buggy
 hook.Add( "PhysgunPickup", "CarKeysVehiclePickingUp", function( ply, ent )
 	if ( table.HasValue( validVehicles, ent:GetClass() ) ) then
 		if ( timer.Exists( "pauseTimer" ) ) then
@@ -64,9 +57,70 @@ hook.Add( "PhysgunPickup", "CarKeysVehiclePickingUp", function( ply, ent )
 		end
 	end
 end )
+]]
+
+validVehicles = {
+	"prop_vehicle_jeep",
+	"prop_vehicle_airboat",
+	"gmod_sent_vehicle_fphysics_base",
+	"prop_vehicle_prisoner_pod",
+	"sent_sakarias_car_banshee",
+	"sent_sakarias_car_belair",
+	"sent_sakarias_car_bobcat",
+	"sent_sakarias_car_bus",
+	"sent_sakarias_car_cadillac",
+	"sent_sakarias_car_camaro",
+	"sent_sakarias_car_chaos126p",
+	"sent_sakarias_car_chevy66",
+	"sent_sakarias_car_clydesdale",
+	"sent_sakarias_car_comet",
+	"sent_sakarias_car_delorean",
+	"sent_sakarias_car_fordgt",
+	"sent_sakarias_car_hedgehog",
+	"sent_sakarias_car_hummer",
+	"sent_sakarias_car_humvee",
+	"sent_sakarias_car_huntley",
+	"sent_sakarias_car_impala88",
+	"sent_sakarias_car_junker4",
+	"sent_sakarias_car_mothtruck",
+	"sent_sakarias_car_mustang",
+	"sent_sakarias_car_police",
+	"sent_sakarias_car_ratmobile",
+	"sent_sakarias_car_sabre",
+	"sent_sakarias_car_stagpickup",
+	"sent_sakarias_car_studebaker",
+	"sent_sakarias_car_taxi",
+	"sent_sakarias_car_1996corvette",
+	"sent_sakarias_car_abrams",
+	"sent_sakarias_car_brera",
+	"sent_sakarias_car_carreragt",
+	"sent_sakarias_car_dodgeram",
+	"sent_sakarias_car_ferrari360",
+	"sent_sakarias_car_ferrarif50",
+	"sent_sakarias_car_ferrarif430",
+	"sent_sakarias_car_fordmustangtr",
+	"sent_sakarias_car_ilcar",
+	"sent_sakarias_car_jimmygibbsjr",
+	"sent_sakarias_car_lambodiablo",
+	"sent_sakarias_car_lambodicop",
+	"sent_sakarias_car_lambomurci",
+	"sent_sakarias_car_lambomurcicop",
+	"sent_sakarias_car_nissanskyline",
+	"sent_sakarias_car_paganizonda",
+	"sent_sakarias_car_porsche911",
+	"sent_sakarias_car_porsche911cop",
+	"sent_sakarias_car_suprarz",
+	"sent_sakarias_car_toyotagtone",
+	"sent_sakarias_car_vipercc",
+	"sent_sakarias_car_vipersrt10",
+	"sent_sakarias_car_yamahayfz450",
+	"sent_sakarias_car_junker3",
+	"sent_sakarias_car_junker2",
+	"sent_sakarias_car_junker1",
+}
 
 hook.Add( "CanPlayerEnterVehicle", "CarKeysVehicleEntering", function( player, vehicle, sRole )
-	if ( SERVER and table.HasValue( validVehicles, vehicle ) ) then
+	if ( SERVER and table.HasValue( validVehicles, vehicle:GetClass() ) ) then
 		if ( player:GetEyeTrace().Entity:GetNWBool( "vehicleLocked", false ) ) then
 			player:ChatPrint("This vehicle is locked, You cannot enter it.")
 			return false
--- lua/weapons/carkeys/shared.lua
+++ lua/weapons/carkeys/shared.lua
@@ -9,16 +9,8 @@ SWEP.Category = "Car Keys"
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
 
-SWEP.SetHoldType = "normal"
-SWEP.SetWeaponHoldType = "normal"
-SWEP.ViewModelFOV = 70
-SWEP.ViewModelFlip = false
-SWEP.UseHands = false
-SWEP.ViewModel = "models/weapons/v_pistol.mdl"
-SWEP.WorldModel = ""
-SWEP.ShowViewModel = false
-SWEP.ShowWorldModel = false
-SWEP.ViewModelBoneMods = {}
+SWEP.ViewModel = "models/weapons/v_crowbar.mdl"
+SWEP.WorldModel = "models/weapons/w_crowbar.mdl"
 
 SWEP.Primary.ClipSize = -1
 SWEP.Primary.DefaultClip = -1
@@ -32,13 +24,63 @@ SWEP.Secondary.Ammo = "none"
 
 validVehicles = {
 	"prop_vehicle_jeep",
-	"prop_vehicle_airboat"
+	"prop_vehicle_airboat",
+	"gmod_sent_vehicle_fphysics_base",
+	"prop_vehicle_prisoner_pod",
+	"sent_sakarias_car_banshee",
+	"sent_sakarias_car_belair",
+	"sent_sakarias_car_bobcat",
+	"sent_sakarias_car_bus",
+	"sent_sakarias_car_cadillac",
+	"sent_sakarias_car_camaro",
+	"sent_sakarias_car_chaos126p",
+	"sent_sakarias_car_chevy66",
+	"sent_sakarias_car_clydesdale",
+	"sent_sakarias_car_comet",
+	"sent_sakarias_car_delorean",
+	"sent_sakarias_car_fordgt",
+	"sent_sakarias_car_hedgehog",
+	"sent_sakarias_car_hummer",
+	"sent_sakarias_car_humvee",
+	"sent_sakarias_car_huntley",
+	"sent_sakarias_car_impala88",
+	"sent_sakarias_car_junker4",
+	"sent_sakarias_car_mothtruck",
+	"sent_sakarias_car_mustang",
+	"sent_sakarias_car_police",
+	"sent_sakarias_car_ratmobile",
+	"sent_sakarias_car_sabre",
+	"sent_sakarias_car_stagpickup",
+	"sent_sakarias_car_studebaker",
+	"sent_sakarias_car_taxi",
+	"sent_sakarias_car_1996corvette",
+	"sent_sakarias_car_abrams",
+	"sent_sakarias_car_brera",
+	"sent_sakarias_car_carreragt",
+	"sent_sakarias_car_dodgeram",
+	"sent_sakarias_car_ferrari360",
+	"sent_sakarias_car_ferrarif50",
+	"sent_sakarias_car_ferrarif430",
+	"sent_sakarias_car_fordmustangtr",
+	"sent_sakarias_car_ilcar",
+	"sent_sakarias_car_jimmygibbsjr",
+	"sent_sakarias_car_lambodiablo",
+	"sent_sakarias_car_lambodicop",
+	"sent_sakarias_car_lambomurci",
+	"sent_sakarias_car_lambomurcicop",
+	"sent_sakarias_car_nissanskyline",
+	"sent_sakarias_car_paganizonda",
+	"sent_sakarias_car_porsche911",
+	"sent_sakarias_car_porsche911cop",
+	"sent_sakarias_car_suprarz",
+	"sent_sakarias_car_toyotagtone",
+	"sent_sakarias_car_vipercc",
+	"sent_sakarias_car_vipersrt10",
+	"sent_sakarias_car_yamahayfz450",
+	"sent_sakarias_car_junker3",
+	"sent_sakarias_car_junker2",
+	"sent_sakarias_car_junker1",
 }
-
-function SWEP:Deploy()
-	local ply = self.Owner
-	ply:DrawViewModel( false )
-end
 
 function SWEP:Reload()
 	if ( SERVER and IsFirstTimePredicted() ) then
@@ -53,11 +95,11 @@ function SWEP:Reload()
 		local trace = ply:GetEyeTrace()
 		if not ( table.HasValue( validVehicles, trace.Entity:GetClass() ) ) then return false end
 		
-		if ( trace.Entity:GetNWString( "vehicleOwner", "nil" ) == "nil" ) then
+		if ( trace.Entity:GetNWString( "vehicleOwner", "N/A" ) == "N/A" ) then
 			trace.Entity:SetNWString( "vehicleOwner", ply:Nick() )
 			ply:EmitSound("ambient/machines/keyboard6_clicks.wav")
 		else
-			trace.Entity:SetNWString( "vehicleOwner", "nil" )
+			trace.Entity:SetNWString( "vehicleOwner", "N/A" )
 			ply:EmitSound("buttons/lightswitch2.wav")
 		end
 	end
@@ -76,7 +118,7 @@ function SWEP:PrimaryAttack()
 		local trace = ply:GetEyeTrace()
 		if not ( table.HasValue( validVehicles, trace.Entity:GetClass() ) ) then return false end
 
-		if ( trace.Entity:GetNWString( "vehicleOwner", "nil" ) == ply:Nick() ) then
+		if ( trace.Entity:GetNWString( "vehicleOwner", "N/A" ) == ply:Nick() ) then
 			ply:EmitSound("npc/metropolice/gear" .. math.floor( math.Rand( 1, 7 ) ) .. ".wav")
 			trace.Entity:SetNWBool( "vehicleLocked", true )
 			ply:AnimRestartGesture( GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_GMOD_GESTURE_ITEM_PLACE, true )
@@ -100,7 +142,7 @@ function SWEP:SecondaryAttack()
 		local trace = ply:GetEyeTrace()
 		if not ( table.HasValue( validVehicles, trace.Entity:GetClass() ) ) then return false end
 	 
-		if ( trace.Entity:GetNWString( "vehicleOwner", "nil" ) == ply:Nick() ) then
+		if ( trace.Entity:GetNWString( "vehicleOwner", "N/A" ) == ply:Nick() ) then
 			ply:EmitSound("npc/metropolice/gear" .. math.floor( math.Rand( 1, 7 ) ) .. ".wav")
 			trace.Entity:SetNWBool( "vehicleLocked", false )
 			ply:AnimRestartGesture( GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_GMOD_GESTURE_ITEM_PLACE, true )
