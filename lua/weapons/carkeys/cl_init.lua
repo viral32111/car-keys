2956ec4fa9c4278b29a045846e562afb92c33c4a
viral32111 <pepper1919@hotmail.co.uk>
2017-03-09 18:32:29 +0000

    Updated to 1.0.8

--- lua/weapons/carkeys/cl_init.lua
+++ lua/weapons/carkeys/cl_init.lua
@@ -14,18 +14,19 @@ function SWEP:DrawHUD()
 	local owner = trace:GetNWString( "vehicleOwner", "N/A" )
 
 	if ( trace == nil ) then return end
+	if ( ply:InVehicle() ) then return end
 
 	if ( table.HasValue( validVehicles, tostring( ply:GetEyeTrace().Entity:GetClass() ) ) ) then
 		if ( owner != "N/A" ) then
-			draw.DrawText( "Owned by " .. owner, "TargetID", ScrW()/2, ScrH()/2+15, Color(255, 255, 255), TEXT_ALIGN_CENTER)
+			draw.DrawText( "Owned by " .. owner, "TargetID", ScrW()/2, ScrH()/2+15, Color( 255, 255, 255 ), TEXT_ALIGN_CENTER)
 			if ( ply:GetEyeTrace().Entity:GetNWBool( "vehicleLocked", false ) ) then
-				draw.DrawText( "Vehicle is locked", "TargetIDSmall", ScrW()/2, ScrH()/2+35, Color(255, 255, 255), TEXT_ALIGN_CENTER)
+				draw.DrawText( "Vehicle is locked", "TargetIDSmall", ScrW()/2, ScrH()/2+35, Color( 255, 255, 255 ), TEXT_ALIGN_CENTER)
 			else
-				draw.DrawText( "Vehicle is unlocked", "TargetIDSmall", ScrW()/2, ScrH()/2+35, Color(255, 255, 255), TEXT_ALIGN_CENTER)
+				draw.DrawText( "Vehicle is unlocked", "TargetIDSmall", ScrW()/2, ScrH()/2+35, Color( 255, 255, 255 ), TEXT_ALIGN_CENTER)
 			end
 		else
-			draw.DrawText( "Vehicle is unowned!", "TargetID", ScrW()/2, ScrH()/2+15, Color(255, 255, 255), TEXT_ALIGN_CENTER)
-			draw.DrawText( "Press R to acquire it", "TargetIDSmall", ScrW()/2, ScrH()/2+35, Color(255, 255, 255), TEXT_ALIGN_CENTER)
+			draw.DrawText( "Vehicle is unowned!", "TargetID", ScrW()/2, ScrH()/2+15, Color( 255, 255, 255 ), TEXT_ALIGN_CENTER)
+			draw.DrawText( "Press R to purchase it", "TargetIDSmall", ScrW()/2, ScrH()/2+35, Color( 255, 255, 255 ), TEXT_ALIGN_CENTER)
 		end
 	end
 end
