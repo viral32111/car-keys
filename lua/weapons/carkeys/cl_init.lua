4836ca2f4839892e6c85a0ac7ced0ab5ef82df17
viral32111 <19510403+viral32111@users.noreply.github.com>
2017-06-01 12:31:16 +0100

    Added keys model

--- lua/weapons/carkeys/cl_init.lua
+++ lua/weapons/carkeys/cl_init.lua
@@ -9,6 +9,16 @@ SWEP.SlotPos = 1
 SWEP.DrawAmmo = false
 SWEP.DrawCrosshair = false
 
+SWEP.HoldType = "melee"
+SWEP.ViewModelFOV = 70
+SWEP.ViewModelFlip = false
+SWEP.UseHands = true
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = true
+SWEP.ViewModelBoneMods = {
+	["Box001"] = { scale = Vector(3, 3, 3), pos = Vector(30, -15.37, -14.631), angle = Angle(-34.445, -81.112, -41.112) }
+}
+
 function SWEP:DrawHUD()
 	local ply = LocalPlayer()
 	local trace = ply:GetEyeTrace().Entity
