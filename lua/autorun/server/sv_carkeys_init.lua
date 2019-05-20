2e5a0819d65d986b98f4b40d6ba41d4adb7a3572
viral32111 <19510403+viral32111@users.noreply.github.com>
2019-05-21 00:51:44 +0100

    Updated Commit SHA.

--- lua/autorun/server/sv_carkeys_init.lua
+++ lua/autorun/server/sv_carkeys_init.lua
@@ -13,7 +13,7 @@ limitations under the License.
 ---------------------------------------------------------------------------]]
 
 -- Current commit of the addon.
-local currentCommit = "efa3af5"
+local currentCommit = "a7735e5"
 
 -- Add config file to client download queue.
 AddCSLuaFile("autorun/shared/sh_carkeys_config.lua")
